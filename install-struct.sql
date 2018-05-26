4115b5de4c2961d4df745626f76164b4e408371d
Konstantin Polyakov <shyzik_93@mail.ru>
2018-05-27 00:20:19 +0300

    Fields "is_copy_for" and "prod_link" were added

--- install-struct.sql
+++ install-struct.sql
@@ -29,7 +29,8 @@ CREATE TABLE `{TABLE_PREFIX}mod_wbs_minishop_products` (
     `prod_count` INT(11) NOT NULL DEFAULT '0',
     `prop_value_ids` JSON,
     `prod_is_hit` INT(11),
-    `is_copy_for` INT(11) NOT NULL DEFAULT '0'
+    `is_copy_for` INT(11) NOT NULL DEFAULT '0',
+    `prod_link` VARCHAR(255) NOT NULL
     PRIMARY KEY (prod_id)
 ){TABLE_ENGINE=MyISAM};
 
