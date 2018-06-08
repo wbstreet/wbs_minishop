918b0df2abcd15f99b198a03966b9423fea96494
Konstantin Polyakov <shyzik_93@mail.ru>
2018-06-08 11:22:59 +0300

    Uninstall is universal now.

--- uninstall.php
+++ uninstall.php
@@ -13,9 +13,13 @@
  // prevent this file from being accessed directly
 if(!defined('WB_PATH')) die(header('Location: index.php'));  
 
-include(__DIR__.'/lib.class.minishop.php');
-$clsModMinishop = new ModMinishop(null, null);
-$r = $clsModMinishop->uninstall();
+$name = substr(basename(__DIR__), 4);
+$class_name = "Mod".ucfirst(str_replace('_', '', ucwords($name, '_'))); // portal_obj_blog into ModPortalObjBlog
+
+include(__DIR__."/lib.class.$name.php");
+$GLOBALS['cls'.$class_name] = new $class_name(0, 0);
+
+$r = $GLOBALS['cls'.$class_name]->uninstall();
 if (gettype($r) === 'string') {
     $admin->print_error($r);
 }
